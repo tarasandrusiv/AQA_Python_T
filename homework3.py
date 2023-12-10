72b5d3788f92ccc919e2897e19e00d767d372623
tarasandrusiv <taras.andrusiv@teamvoy.com>
2023-12-11 00:17:04 +0200

    fixed some comments

--- homework3.py
+++ homework3.py
@@ -1,12 +1,13 @@
-"""Homework3."""
+"""H3. Modify old style format into #1 string with format() and #2 f-string."""
 
-norway_text_format_method = """Automatisering akselererer {0}yeblikket da roboter 
-        vil erobre planeten v{1}r. ({2})""".format('ø', 'å', 'Æ')
+# 1 string with format()
+norway_text_format_method = 'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ')
 print(norway_text_format_method)
 
+# 2 f-string
 first_var = 'ø'
 second_var = 'å'
 third_var = 'Æ'
-print(f"""Automatisering akselererer {first_var}yeblikket da roboter
-        vil erobre planeten v{second_var}r. ({third_var})""")
-
+fstring = (f'Automatisering akselererer {first_var}yeblikket da '
+           f'roboter vil erobre planeten v{second_var}r. ({third_var})')
+print(fstring)
