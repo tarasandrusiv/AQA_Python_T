b79f77ea24fa6a8e70f346d0242924f917a6e474
tarasandrusiv <taras.andrusiv@teamvoy.com>
2023-12-10 13:22:31 +0200

    homework3

---
+++ homework3.py
@@ -0,0 +1,12 @@
+"""Homework3."""
+
+norway_text_format_method = """Automatisering akselererer {0}yeblikket da roboter 
+        vil erobre planeten v{1}r. ({2})""".format('ø', 'å', 'Æ')
+print(norway_text_format_method)
+
+first_var = 'ø'
+second_var = 'å'
+third_var = 'Æ'
+print(f"""Automatisering akselererer {first_var}yeblikket da roboter
+        vil erobre planeten v{second_var}r. ({third_var})""")
+
