66fbbca55cc15e7dc6bb1374ea3974ca666ddae7
tarasandrusiv <taras.andrusiv@teamvoy.com>
2023-12-16 16:25:22 +0200

    fixed long string errors

--- homework3.py
+++ homework3.py
@@ -1,7 +1,10 @@
 """H3. Modify old style format into #1 string with format() and #2 f-string."""
 
 # 1 string with format()
-norway_text_format_method = 'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ')
+norway_text_format_method = ('Automatisering akselererer '
+                             '{0}yeblikket da roboter vil '
+                             'erobre planeten '
+                             'v{1}r. ({2})').format('ø', 'å', 'Æ')
 print(norway_text_format_method)
 
 # 2 f-string
@@ -11,3 +14,7 @@ third_var = 'Æ'
 fstring = (f'Automatisering akselererer {first_var}yeblikket da '
            f'roboter vil erobre planeten v{second_var}r. ({third_var})')
 print(fstring)
+
+
+
+
