329a628a07033b977951c5df397ea1d28692af69
tarasandrusiv <taras.andrusiv@teamvoy.com>
2023-12-20 19:18:21 +0200

    sorting list2

--- homework6.py
+++ homework6.py
@@ -2,10 +2,22 @@
 Homework #6 List sorting.
 
 Task description:
+1. Need to sort list by age and sex fields
+2. Need to get new list as old list without'
+ 'first two elements and last two elements. Print this new list'
+3. In the new list need to calculate total numbers of'
+ '"female"  and "male" and print it as small table.'
 
 Flow:
- -
+ 1. Sorting list using sort method and lambda build in function
+ 2. using pop method by indexes for removing extra list elements'
+  'from the list. printing new list'
+ 3. importing itertools to format list as flatlist
+ 4. then counting each 'female' and 'male' from flatlist
+ 5. creating table using loop and display count results
+
 """
+import itertools
 
 people = [
     ('Alice', 32, 100, 'Johnson', 'female'),
@@ -49,5 +61,34 @@ people = [
     ('Mike', 55, 55, 'Johnson', 'male')
 ]
 
-s1 = people.sort(key=lambda x: x[0] + x[4])
-print(s1)
+people.sort(key=lambda x: (x[1], x[4]))
+print(people)
+
+people_new = people.copy()
+people_new.pop(0)
+people_new.pop(0)
+people_new.pop()
+people_new.pop()
+print(people_new)
+
+flat_people = list(itertools.chain.from_iterable(people_new))
+count_fem = flat_people.count('female')
+count_male = flat_people.count('male')
+
+print(count_fem)
+print(count_male)
+
+sep_num = 21
+
+print("-" * sep_num)
+print(f"| {'vowel':^7} | {'count':^7} |")
+print("-" * sep_num)
+sex_count = {
+    "female": count_fem,
+    "male": count_male,
+}
+
+for sex, count in sex_count.items():
+    print(f"| {sex:^8}| {count:^7} |")
+
+print("-" * sep_num)
